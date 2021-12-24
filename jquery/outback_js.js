3d63694d20ccb04c50c4d8bc93dd88c63f8a8b51
kwonch <rnjsskfen153@naver.com>
2021-12-24 16:02:32 +0900

    2021-12-24
    section1 작업완료 아웃백에 대해
    그리고 아웃백 딜리버리 내용
    
    section2 는 리뷰 슬라이드 효과 적용중

--- jquery/outback_js.js
+++ jquery/outback_js.js
@@ -12,3 +12,20 @@
     });
 
   });
+  var slideIndex = 0;
+  showSlides();
+  
+  function showSlides() {
+    var i;
+    var slides = document.getElementsByClassName("review_slide");
+ 
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+    }
+   
+    
+   
+    slides[slideIndex-1].style.display = "block";  
+    
+    setTimeout(showSlides, 2000); // Change image every 2 seconds
+  }
